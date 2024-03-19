778a93703ec01d59b617b82b419c8ff100d03c26
Fabio Nettis <nettisfabio@gmail.com>
2024-03-19 14:20:02 +0100

    feat: implement stratagem generation

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import planets from "routes/planets";
 import sectors from "routes/sectors";
 import attacks from "routes/attacks";
 import factions from "routes/factions";
+import stratagems from "routes/stratagems";
 
 // initiate hono api
 const app = new Hono().basePath("/api");
@@ -19,7 +20,16 @@ app.use(rateLimit);
 app.use(cache);
 
 // routes for the api
-const routes = [planets, sectors, wars, factions, attacks, events, orders];
-for (const route of routes) await route(app);
+const routes = [
+  planets,
+  sectors,
+  wars,
+  factions,
+  attacks,
+  events,
+  orders,
+  stratagems,
+];
 
+for (const route of routes) await route(app);
 export default app;
