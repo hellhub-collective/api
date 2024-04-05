4a5c52a2b05fd3c0a96cb07276e3188cf36663bb
Fabio Nettis <nettisfabio@gmail.com>
2024-04-05 15:31:49 +0200

    chore(types): add types for 3rd party history api

---
+++ src/types/history.ts
@@ -0,0 +1,8 @@
+export interface HistoryEntry {
+  planetIndex: number;
+  maxHealth: number;
+  liberation: number;
+  currentHealth: number;
+  previousLiberation: number;
+  liberationChange: number;
+}
