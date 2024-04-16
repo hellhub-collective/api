ded86d22fa77d138d210da61a94a6381ca9f5bb1
Fabio Nettis <nettisfabio@gmail.com>
2024-04-16 16:48:31 +0200

    refractor: remove obsolete sentry utilities

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,36 +1,19 @@
-import * as Sentry from "@sentry/node";
+import * as Sentry from "@sentry/bun";
 
-/**
- * Capture an exception and log it to the console, if no DSN is provided,
- * it will not be sent to Sentry.
- */
-export default function captureException(
-  error: Error,
-  log: boolean = process.env.NODE_ENV !== "production",
-): void {
-  if (log) console.error(error);
-  if (!process.env.SENTRY_DSN) return;
-  if (process.env.NODE_ENV !== "production") return;
-  Sentry.captureException(error);
-}
+export const sentryOptions: Sentry.BunOptions = {
+  integrations: [],
+  tracesSampleRate: 0.6,
+  dsn: process.env.SENTRY_DSN,
+  release: process.env.KOYEB_GIT_SHA,
+  environment: process.env.NODE_ENV ?? "development",
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/api-hellhub-collective\.koyeb\.app\/api/,
+  ],
+};
 
-/**
- * Initialize Sentry with the provided DSN, if no DSN is provided, it will
- * not be enabled. By the default, it will not be enabled in development as
- * well.
- */
 export function initSentry(): void {
   if (process.env.NODE_ENV !== "production") return;
   if (!process.env.SENTRY_DSN) return;
-  Sentry.init({
-    integrations: [],
-    tracesSampleRate: 0.6,
-    dsn: process.env.SENTRY_DSN,
-    release: process.env.KOYEB_GIT_SHA,
-    environment: process.env.NODE_ENV ?? "development",
-    tracePropagationTargets: [
-      "localhost",
-      /^https:\/\/api-hellhub-collective\.koyeb\.app\/api/,
-    ],
-  });
+  Sentry.init(sentryOptions);
 }
