056db1319e92caca2e5e6848ea7a60dc6f98c35f
Fabio Nettis <nettisfabio@gmail.com>
2024-04-25 09:50:18 +0200

    refractor: implement new classes based on sqlite

--- src/middleware/rate-limit.ts
+++ src/middleware/rate-limit.ts
@@ -1,21 +1,14 @@
 import type { Context, Next } from "hono";
 
-import RateLimitCache from "classes/rate-limit";
+import RateLimiter from "classes/rate-limit";
 
 const RATE_LIMIT = parseInt(process.env.RATE_LIMIT || "60");
 
-interface RateLimit {
-  count: number;
-  reset: number;
-  limit: number;
-  remaining: number;
-}
-
 export default async function rateLimit(ctx: Context, next: Next) {
-  const ip = ctx.req.header("X-Forwarded-For") || ctx.req.header("X-Real-IP");
-  const key = `rate-limit:${ip ?? "unknown"}`;
-  const rl = RateLimitCache.get<RateLimit>(key);
+  const ip =
+    ctx.req.header("X-Forwarded-For") || ctx.req.header("X-Real-IP") || "anon";
 
+  const rl = RateLimiter.get(ip);
   const now = Date.now();
   const reset = now + 1000 * 60;
 
@@ -25,25 +18,30 @@ export default async function rateLimit(ctx: Context, next: Next) {
   ctx.res.headers.set("X-Rate-Remaining", `${rl?.remaining ?? RATE_LIMIT}`);
 
   if (!rl) {
-    const payload: RateLimit = {
-      count: 1,
-      reset: reset,
-      limit: RATE_LIMIT,
-      remaining: RATE_LIMIT - 1,
-    };
+    RateLimiter.set(
+      {
+        ip,
+        count: 1,
+        reset: reset,
+        threshold: RATE_LIMIT,
+        remaining: RATE_LIMIT - 1,
+      },
+      60,
+    );
 
-    RateLimitCache.set(key, payload, 60);
     return await next();
   }
 
   if (rl.count < RATE_LIMIT) {
-    const payload: RateLimit = {
-      ...rl,
-      count: rl.count + 1,
-      remaining: rl.limit - (rl.count + 1),
-    };
+    RateLimiter.set(
+      {
+        ...rl,
+        count: rl.count + 1,
+        remaining: rl.threshold - (rl.count + 1),
+      },
+      60,
+    );
 
-    RateLimitCache.set(key, payload, 60);
     return await next();
   }
 
